0a12304494ef05799a1377b3c25da895bcfad0cb
Kaylee Morris <kaylee.christine.morris@gmail.com>
2026-03-01 22:28:56 -0600

    practice v-for looping

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,74 @@
-<!-- Vue 13: V-if -->
+<!-- Vue 14: V-for -->
 <template>
+  <h1>{{ message }}</h1>
+  <button @click="sortUsersByAge">
+    Sort Users by Age
+  </button>
+  <ul>
+    <li v-for="(user, index) in users" :key="user.id">
+      {{ index }} - {{ user.id }} - {{ user.name }} - {{ user.age }} -
+      {{ user.isActive }}
+    </li>
+  </ul>
+
+  <hr>
+
+  <table>
+    <tr>
+      <th>Index</th>
+      <th>Id</th>
+      <th>Name</th>
+      <th>Age</th>
+      <th>Operation</th>
+    </tr>
+    <tr v-for="(user, index) in users" :key="user.id">
+      <td>{{ index }}</td>
+      <td>{{ user.id }}</td>
+      <td :class="{ inactive: !user.isActive }">{{ user.name }}</td>
+      <td>{{ user.age }}</td>
+      <td>
+        <button @click="user.isActive = !user.isActive">
+          {{ user.isActive ? 'Deactivate' : 'Restore' }}
+        </button>
+      </td>
+    </tr>
+  </table>
+
+  <hr>
+
+  <p>
+    Use v-for to iterate through the properties of an object:
+  </p>
+  <div v-for="(value, key, index) in users[0]">
+    {{ index }} - {{ key }}: {{ value }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+let message = ref('Hello, V-for!')
+
+const users = ref([
+  { id: 1001, name: 'John Smith', age: 26, isActive: false },
+  { id: 1002, name: 'Tom Doe', age: 16, isActive: false },
+  { id: 1003, name: 'Frankin Wong', age: 18, isActive: true }
+])
+
+function sortUsersByAge() {
+  users.value.sort((a, b) => a.age - b.age)
+}
+</script>
+
+<style scoped>
+.inactive {
+  color: red;
+  text-decoration: line-through;
+}
+</style>
+
+<!-- Vue 13: V-if -->
+<!-- <template>
   <h1>{{ message }}</h1>
   <p>
     What has keys but can't open locks, 
@@ -13,11 +82,6 @@
   </button>
 
   <div v-if="showAnswer">A keyboard.</div>
-
-  <!-- <div v-if="Math.random() > 0.5">Now you see me</div>
-  <div v-else>Now you don't</div> -->
-
-  <!-- <div v-show="showAnswer">A keyboard.</div> -->
 </template>
 
 <script setup>
@@ -27,7 +91,7 @@ let message = ref('Hello, V-if!')
 let showAnswer = ref(false)
 </script>
 
-<style scoped></style>
+<style scoped></style> -->
 
 <!-- Vue 12: V-Model & V-Bind -->
 <!-- <template>
