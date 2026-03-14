4884f7885696126f0d203fabf19f0e2594783071
Kaylee Morris <kaylee.christine.morris@gmail.com>
2026-03-13 22:33:06 -0500

    practice scoped slots

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,69 @@
-<!-- Vue 28: Named Slots -->
+<!-- Vue 29: Scoped Slots -->
 <template>
+  <StudentList :list="list">
+    <template #default="{ stu }">
+      <span :class="{ cursed: stu.name == 'Harry' }">
+        {{ stu.name }}
+      </span>
+    </template>
+  </StudentList>
+
+  <hr>
+
+  <el-table :data="todoList" stripe border style="width: 100%">
+    <el-table-column prop="userId" label="User ID" width="180" />
+    <el-table-column prop="id" label="ID" width="180" />
+    <el-table-column prop="title" label="Title" />
+    <el-table-column prop="completed" label="Status">
+      <template #default="slotProps">
+        <el-tag type="success" v-if="slotProps.row.completed">Complete</el-tag>
+        <el-tag type="danger" v-else>Incomplete</el-tag>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+import StudentList from './StudentList.vue'
+
+const list = ref([
+  {
+    id: 1,
+    name: 'Harry'
+  },
+  {
+    id: 2,
+    name: 'Hermione'
+  },
+  {
+    id: 3,
+    name: 'Ron'
+  }
+])
+
+const todoList = ref([])
+
+async function getTodoList() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+  const data = await response.json()
+  console.log(data)
+  todoList.value = data
+}
+
+onMounted(() => {
+  getTodoList()
+})
+</script>
+
+<style scoped>
+.cursed {
+  color: red;
+}
+</style>
+
+<!-- Vue 28: Named Slots -->
+<!-- <template>
   <BaseLayout>
     <template #header>
       <h1>Here is a page title.</h1>
@@ -18,7 +82,7 @@
 import BaseLayout from './BaseLayout.vue'
 </script>
 
-<style scoped></style>
+<style scoped></style> -->
 
 <!-- Vue 27: Slots -->
 <!-- <template>
