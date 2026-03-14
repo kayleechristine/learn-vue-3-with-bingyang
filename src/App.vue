0d5a8c834d714d587abd2981e4e37e051095bd89
Kaylee Morris <kaylee.christine.morris@gmail.com>
2026-03-13 20:49:30 -0500

    practice vue props

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,87 @@
-<!-- Vue 21: Intro Components -->
+<!-- Vue 23: Props -->
 <template>
-  <!-- pascal case (best practice): -->
+  <BlogPost
+    id="1"
+    blogPostTitle="What is a muggle?"
+    blogPostContent="A muggle is a person who lacks any sort of magical ability..."
+  >
+  </BlogPost>
+
+  <BlogPost
+    id="1"
+    blog-post-title="What is a muggle?"
+    blog-post-content="A muggle is a person who lacks any sort of magical ability..."
+  >
+  </BlogPost>
+
+  <BlogPost
+    :id="blogPostId"
+    :blog-post-title="blogPostTitle"
+    :blog-post-content="blogPostContent"
+  ></BlogPost>
+
+  <hr />
+
+  <BlogPost
+    v-for="post in posts"
+    :key="post.id"
+    :id="post.id"
+    :blog-post-title="post.blogPostTitle"
+    :blog-post-content="post.blogPostContent"
+  ></BlogPost>
+
+  <hr>
+
+  <BlogPost v-for="post in posts" :key="post.id" v-bind="post"></BlogPost>
+</template>
+
+<script setup>
+import BlogPost from '@/BlogPost.vue'
+import { ref } from 'vue'
+
+let blogPostId = ref(2)
+let blogPostTitle = ref(
+  'HP and the Cursed Child Broadway production suspended until April'
+)
+let blogPostContent = ref(
+  'Adding to the earlier post, the Broadway production of Harry Potter and the Cursed Child has been...'
+)
+
+let posts = ref([
+  {
+    id: 1,
+    blogPostTitle: 'What is a muggle in HP world?',
+    blogPostContent:
+      'A muggle is a person who lacks any sort of magical ability...'
+  },
+  {
+    id: 2,
+    blogPostTitle:
+      'HP and the Cursed Child Broadway production suspended until April',
+    blogPostContent:
+      'Adding to the earlier post, the Broadway production of Harry Potter and the Cursed Child has been...'
+  },
+  {
+    id: 3,
+    blogPostTitle: 'Potter DIY: Make Your Own “Harry Potter Puppet Pal”',
+    blogPostContent:
+      'We all remember those iconic YouTube videos of Harry and the gang and probably couldn’t get “The Mysterious...'
+  },
+  {
+    id: 4,
+    blogPostTitle: 'Fan Project Brings Life to “Harry Potter” in Translation',
+    blogPostContent:
+      'It’s no secret that the Harry Potter series is a global phenomenon, having been translated into over 80 languages to date....'
+  }
+])
+</script>
+
+<style scoped></style>
+
+<!-- Vue 21: Intro Components -->
+<!--<template>
   <ButtonCounter></ButtonCounter>
   <ButtonCounter></ButtonCounter>
-  <!-- kebab case: -->
   <button-counter></button-counter>
 
   <hr>
@@ -20,7 +98,7 @@ import ButtonCounter from './ButtonCounter.vue'
 import ComponentA from '@/ComponentA.vue'
 </script>
 
-<style scoped></style>
+<style scoped></style> -->
 
 <!-- Vue 20: Lifecycle Hooks -->
  <!-- <template>
